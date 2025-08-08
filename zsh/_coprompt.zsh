11eb6ef10f2bea94a299d2ca4d8a2b8e7302198b
mediumshade <nohlson@purdue.edu>
2025-08-08 01:20:28 -0500

    Always flush the ZLE buffer every time shorthand is envoked

--- zsh/_coprompt.zsh
+++ zsh/_coprompt.zsh
@@ -1,6 +1,8 @@
 function _coprompt() {
   local q cmd unsafe_flag=""
   zle -I
+  BUFFER=""
+  CURSOR=0
   printf 'Generate a command: ' > /dev/tty
   IFS= read -r q < /dev/tty
   [[ -z "$q" ]] && return 0
@@ -12,7 +14,8 @@ function _coprompt() {
 
   cmd="$("$HOME/shorthand/bin/termgen" $unsafe_flag --model=cmdgen -- "$q" 2>/dev/null)"
   [[ -z "$cmd" ]] && return 0
-  LBUFFER+="$cmd"
+  BUFFER="$cmd"
+  CURSOR=${#BUFFER}
   zle redisplay
 }
 zle -N _coprompt
